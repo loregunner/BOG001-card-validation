b635bf931129b3c85205f618af833ee26d61beab
loregunner <dahara59@gmail.com>
2020-06-09 09:54:58 -0500

    subidos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+function agregar() {
+    var arrayInput = new Array();
+    var inputsValue = document.getElementsByClassName('creditCarnumberinput');
+    creditoValue = [].map.call(inputsValue, function (dataInput) {
+        arrayInput.push(dataInput.value);
+
+
+    });
+    arrayInput.forEach(function (inputsValue) {
+        console.log("numero en array" + inputsValue);
+    });
+
+
+}
 import validator from './validator.js';
 
 console.log(validator);
+
+
---
+++ src/indexp1.js
@@ -0,0 +1,4 @@
+function categoriaUno() 
+{ window.location.href = "indexP2.html"; }
+function categoriaDos() 
+{ window.location.href = "indexP3.html"; }
