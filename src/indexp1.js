470c7665b4b57457de231d79485e6d936378a22a
loregunner <dahara59@gmail.com>
2020-06-16 12:46:15 -0500

    confirmar

--- src/indexp1.js
+++ src/indexp1.js
@@ -1,4 +1,5 @@
-function categoriaUno() 
-{ window.location.href = "indexP2.html"; }
-function categoriaDos() 
-{ window.location.href = "indexP3.html"; }
+function GpQ(){
+    window.location.href = 'indexP2';
+  }
+  const boton1 = document.getElementById("botonCategoria1");
+  boton.addEventListener("click", GpQ);
