5daecd35e5c5a989bf6c1711c25cbd86254d5594
loregunner <dahara59@gmail.com>
2020-06-16 10:16:42 -0500

    confirmar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-function agregar() {
-    var arrayInput = new Array();
-    var inputsValue = document.getElementsByClassName('creditCarnumberinput');
-    creditoValue = [].map.call(inputsValue, function (dataInput) {
-        arrayInput.push(dataInput.value);
-
-
-    });
-    arrayInput.forEach(function (inputsValue) {
-        console.log("numero en array" + inputsValue);
-    });
-
-
+/*import validator from './validator.js';
+console.log(validator);*/
+function getData() {
+  var nombre = document.getElementById("nombres").value;
+  var creditCardNumber1 = document.getElementById("creditCardNumber").value;
+  // console.log(getData)
+  console.log(nombre, creditCardNumber1);
+  localStorage.setItem("nombre", nombre);
+  localStorage.setItem("creditCardNumber1", creditCardNumber1);
+  const aNumeroTarjeta = Array.from(creditCardNumber1);
+  //console.log(aNumeroTarjeta)
+  const aInvertido = aNumeroTarjeta.reverse();
+  aInvertido.forEach(function (element, index) {
+    index++;
+    if (index % 2 == 0) { element = element * 2 };
+    if (element > 9) { element = element - 9 };
+    console.log(element);
+    var otroArray = element;
+    var suma = 0;
+    /*for (var i = 0; i < otroArray.length; i++) {
+      suma += suma + parseInt(otroArray[i]);
+    }
+    console.log(suma)*/
+  })
+  //  window.location.href = 'indexP4.html' 
 }
-import validator from './validator.js';
-
-console.log(validator);
-
+const boton = document.getElementById("BotonTarjeta");
+boton.addEventListener("click", getData);
 
