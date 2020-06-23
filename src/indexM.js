a4ff012b3690146e92e0981884a957ce13dbde11
loregunner <dahara59@gmail.com>
2020-06-23 17:17:41 -0500

    confirmar

--- src/indexM.js
+++ src/indexM.js
@@ -1,9 +1,7 @@
-const { createTransformer } = require("babel-jest");
-
 let modal = document.getElementById("miModal");
 let flex = document.getElementById("flex");
 let abrir = document.getElementById("abrir1");
-let close = document.getElementById("close");
+let cerrar = document.getElementById("close");
 
 abrir.addEventListener("click", function () {
     modal.style.display = "block";
